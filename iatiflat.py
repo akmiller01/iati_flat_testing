d4a120ec86e0c0d48d5edcb36b264790a6aae171
Alex Miller <alex.k.miller@gmail.com>
2017-11-16 11:29:36 -0500

    Iteration

--- iatiflat.py
+++ iatiflat.py
@@ -52,7 +52,11 @@ def make_versioned_code_dict(version):
     return master_dict
 
 def flatten_datastore_url(url):
-    activities_raw = requests.get(url).content
+    output = []
+    conn = requests.get(url)
+    if conn.status_code!=200:
+        return 500
+    activities_raw = conn.content
     activities = iati.Dataset(activities_raw)
     
     ns =  activities.xml_tree.getroot().nsmap
@@ -60,9 +64,7 @@ def flatten_datastore_url(url):
     
     iati_activities = activities.xml_tree.xpath('iati-activities')[0]
     activity_len = len(iati_activities.findall("iati-activity"))
-    
-    output = []
-    
+        
     bar = progressbar.ProgressBar()
     for i in bar(range(0,activity_len)):
         activity = activities.xml_tree.xpath('iati-activities/iati-activity[%s]' % (i + 1) )[0]
@@ -156,7 +158,23 @@ def flatten_datastore_url(url):
     return output
     
 if __name__ == '__main__':
-    output = flatten_datastore_url("http://datastore.iatistandard.org/api/1/access/activity.xml?limit=1000")
-    data = pd.DataFrame(output)
-    data.columns = ["iati_version","transaction_type","transaction_date","currency","value","value_date","provider_activity_id","sector","recipient_country","recipient_region","flow_type_code","disbursement_channel","finance_type","aid_type_code","budget_or_transaction","budget_type"]
-    data.to_csv("test_output.csv",index=False,encoding="utf-8")
+    output = []
+    i = 0
+    while output != 500:
+        output = flatten_datastore_url("http://datastore.iatistandard.org/api/1/access/activity.xml?limit=1000&offset={}".format(i))
+        if output != 500:
+            data = pd.DataFrame(output)
+            data.columns = ["iati_version","transaction_type","transaction_date","currency","value","value_date","provider_activity_id","sector","recipient_country","recipient_region","flow_type_code","disbursement_channel","finance_type","aid_type_code","budget_or_transaction","budget_type"]
+            data.to_csv("test_output{}.csv".format(i),index=False,encoding="utf-8")
+            i += 1000
+        print(i)
+    #Reduce limit until we get a valid response
+    j = 1000
+    while output == 500 and j > 0:
+        output = flatten_datastore_url("http://datastore.iatistandard.org/api/1/access/activity.xml?limit={}&offset={}".format(j,i))
+        j -= 1
+        print("Server error, reducing the limit to %s" % j)
+    if j > 0:
+        data = pd.DataFrame(output)
+        data.columns = ["iati_version","transaction_type","transaction_date","currency","value","value_date","provider_activity_id","sector","recipient_country","recipient_region","flow_type_code","disbursement_channel","finance_type","aid_type_code","budget_or_transaction","budget_type"]
+        data.to_csv("test_output{}.csv".format(i),index=False,encoding="utf-8")
