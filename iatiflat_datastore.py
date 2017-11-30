5a33ccab84fe14ebfdc3fed5887a9e59cd9fb8e6
Alex Miller <alex.k.miller@gmail.com>
2017-11-30 15:51:41 -0500

    Split out datastore version and static data version

--- iatiflat_datastore.py
+++ iatiflat_datastore.py
@@ -1,12 +1,10 @@
 import iati.default
+import requests
 import progressbar
 import pandas as pd
 from collections import defaultdict
 import pdb
 import math
-from lxml import etree
-import os
-from io import StringIO
 
 #Probably need to refactor this for multiple sectors, providers, etc.
 def default_first(array):
@@ -55,19 +53,54 @@ def make_versioned_code_dict(version):
         }
     return master_dict
 
-def flatten_activities(root):
+def validate_datastore_url(url):
+    conn = requests.get(url)
+    if conn.status_code!=200:
+        return 500
+    else:
+        return 200
+
+def recursiveSearch(curPos,lastPos,lastOutput):
+    output = validate_datastore_url("http://datastore.iatistandard.org/api/1/access/activity.xml?limit={}&offset={}".format(curPos,i))
+    delta = int(math.ceil(abs((curPos-lastPos)/2)))
+    #Stopped moving with valid output
+    if (curPos-lastPos)==0 and output!=500:
+        return curPos
+    #Last move was from invalid to valid with a delta of 1
+    elif lastOutput == 500 and output != 500 and abs(curPos-lastPos)==1:
+        return curPos
+    elif delta==0:
+        delta = 1
+    lastPos = curPos
+    print("Guess: {} is {}, moving by {}".format(curPos,output!=500,delta))
+    if output==500:
+        curPos -= delta
+        return recursiveSearch(curPos,lastPos,lastOutput)
+    else:
+        curPos += delta
+        return recursiveSearch(curPos,lastPos,lastOutput)
+
+def flatten_datastore_url(url):
     output = []
-    try:
-        version = root.attrib["version"]
-    except KeyError:
-        #Default?
-        version = '2.02'
+    conn = requests.get(url)
+    if conn.status_code!=200:
+        return 500
+    activities_raw = conn.content
+    activities = iati.Dataset(activities_raw)
     
-    activity_len = len(root.findall("iati-activity"))
+    ns =  activities.xml_tree.getroot().nsmap
+    iati_extra = ns['iati-extra']
+    
+    iati_activities = activities.xml_tree.xpath('iati-activities')[0]
+    activity_len = len(iati_activities.findall("iati-activity"))
         
     bar = progressbar.ProgressBar()
     for i in bar(range(0,activity_len)):
-        activity = root.xpath('iati-activity[%s]' % (i + 1) )[0]
+        activity = activities.xml_tree.xpath('iati-activities/iati-activity[%s]' % (i + 1) )[0]
+        try:
+            version = activity.attrib["{%s}version" % iati_extra]
+        except KeyError:
+            version = "2.01"
         
         try:
             vcd = make_versioned_code_dict(version)
@@ -75,27 +108,22 @@ def flatten_activities(root):
             continue
             
         child_tags = [child.tag for child in activity.getchildren()]
-
-        secondary_reporter = default_first(activity.xpath("reporting-org/@secondary-reporter"))
-        #Set up defaults
-        
-        #default_donor_org
-        default_donor_code = None
-        default_donor_text = None
-        default_donor_type = None
+        #participating orgs
+        funding_org_ref = None
+        funding_org_text = None
+        funding_org_type = None
         if "participating-org" in child_tags:
             participating_orgs = activity.findall("participating-org")
             for p_org in participating_orgs:
                 p_org_role = default_first(p_org.xpath("@role"))
                 #Ignoring possibility of multiple funders
                 if p_org_role==1 or p_org_role=="Funding":
-                    default_donor_code = default_first(p_org.xpath("@ref"))
-                    default_donor_text = default_first(p_org.xpath("text()"))
-                    default_donor_type = default_first(p_org.xpath("@type"))
-                    
-        #Add crs-add (channel code, ftc, pba) and descriptions here 
+                    funding_org_ref = default_first(p_org.xpath("@ref"))
+                    funding_org_text = default_first(p_org.xpath("text()"))
+                    funding_org_type = default_first(p_org.xpath("@type"))
+        #Set up defaults?
         defaults = {}
-        default_tags = ["default-currency","default-finance-type","default-aid-type","default-flow-type","recipient-country","recipient-region","sector"]
+        default_tags = ["default-currency","default-flow-type","recipient-country","recipient-region","sector"]
         for tag in default_tags:
             if tag in activity.attrib.keys():
                 defaults[tag] = activity.attrib[tag]
@@ -103,18 +131,14 @@ def flatten_activities(root):
                 defaults[tag] = default_first(activity.xpath("{}/@code".format(tag)))
             else:
                 defaults[tag] = None
-        
         has_transactions = "transaction" in child_tags
-        if "crs-add" in child_tags:
-            pdb.set_trace()
         if has_transactions:
             transactions = activity.findall("transaction")
             for transaction in transactions:
                 transaction_type_code = default_first(transaction.xpath("transaction-type/@code"))
-                # transaction_type = recode_if_not_none(transaction_type_code,vcd["TransactionType"])
+                transaction_type = recode_if_not_none(transaction_type_code,vcd["TransactionType"])
                 
                 transaction_date = default_first(transaction.xpath("transaction-date/@iso-date"))
-                year = transaction_date[:4] if transaction_date else None
                 
                 currency = default_first(transaction.xpath("value/@currency"))
                 currency = replace_default_if_none(currency,defaults["default-currency"])
@@ -122,29 +146,31 @@ def flatten_activities(root):
                 value = default_first(transaction.xpath("value/text()"))
                 value_date = default_first(transaction.xpath("value/@value-date"))
                 
-                transaction_donor_code = default_first(transaction.xpath("provider-org/@ref"))
-                transaction_donor_text = default_first(transaction.xpath("provider-org/text()"))
+                transaction_provider_org_ref = default_first(transaction.xpath("provider-org/@ref"))
+                transaction_provider_org_text = default_first(transaction.xpath("provider-org/text()"))
+                transaction_provider_org_type = default_first(transaction.xpath("provider-org/@type"))
                 
                 #Introduce some sort of check here, to see if we've replaced one then we've replaced them all
-                donor_code = replace_default_if_none(transaction_donor_code,default_donor_code)
-                donor_text = replace_default_if_none(transaction_donor_text,default_donor_text)
+                provider_org_ref = replace_default_if_none(transaction_provider_org_ref,funding_org_ref)
+                provider_org_text = replace_default_if_none(transaction_provider_org_text,funding_org_text)
+                provider_org_type = replace_default_if_none(transaction_provider_org_type,funding_org_type)
 
                 provider_activity_id = default_first(transaction.xpath("provider-org/@provider-activity-id"))
                 
                 sector_code = default_first(transaction.xpath("sector/@code"))
                 sector_code = replace_default_if_none(sector_code,defaults["sector"])
                 
-                # sector = recode_if_not_none(sector_code,vcd["Sector"])
+                sector = recode_if_not_none(sector_code,vcd["Sector"])
                 
                 recipient_country_code = default_first(transaction.xpath("recipient-country/@code"))
                 recipient_country_code = replace_default_if_none(recipient_country_code,defaults["recipient-country"])
     
-                # recipient_country = recode_if_not_none(recipient_country_code,vcd["Country"])
+                recipient_country = recode_if_not_none(recipient_country_code,vcd["Country"])
                 
                 recipient_region_code = default_first(transaction.xpath("recipient-region/@code"))
                 recipient_region_code = replace_default_if_none(recipient_region_code,defaults["recipient-region"])
     
-                # recipient_region = recode_if_not_none(recipient_region_code,vcd["Region"])
+                recipient_region = recode_if_not_none(recipient_region_code,vcd["Region"])
                 
                 flow_type_code = default_first(transaction.xpath("flow-type/@code"))
                 flow_type_code = replace_default_if_none(flow_type_code,defaults["default-flow-type"])
@@ -152,17 +178,11 @@ def flatten_activities(root):
                 disbursement_channel_code = default_first(transaction.xpath("disbursement-channel/@code"))
                 
                 finance_type_code = default_first(transaction.xpath("finance-type/@code"))
-                finance_type_code = replace_default_if_none(finance_type_code,defaults["default-finance-type"])
                 
                 aid_type_code = default_first(transaction.xpath("aid-type/@code"))
-                aid_type_code = replace_default_if_none(aid_type_code,defaults["default-aid-type"])
-                # aid_type = recode_if_not_none(aid_type_code,vcd["AidType"])
+                aid_type = recode_if_not_none(aid_type_code,vcd["AidType"])
                 
-                category = None
-                budget_type = None
-                b_or_t = "Transaction"
-                
-                row = [version,secondary_reporter,year,transaction_date,donor_code,donor_text,recipient_country_code,recipient_region_code,flow_type_code,category,finance_type_code,currency,value,aid_type_code,sector_code,disbursement_channel_code,b_or_t,budget_type]
+                row = [version,transaction_type,transaction_date,currency,value,value_date,provider_org_ref,provider_org_text,provider_org_type,provider_activity_id,sector,recipient_country,recipient_region,flow_type_code,disbursement_channel_code,finance_type_code,aid_type,"Transaction",None]
                 output.append(row)
             
             has_budget = "budget" in child_tags
@@ -173,55 +193,47 @@ def flatten_activities(root):
                         budget_type = budget.attrib["type"]
                     else:
                         budget_type = None
-                        
-                    transaction_date = default_first(budget.xpath("period-start/@iso-date"))
-                    year = transaction_date[:4] if transaction_date else None
-                        
                     value = default_first(budget.xpath("value/text()"))
                     value_date = default_first(budget.xpath("value/@value-date"))
                     currency = default_first(budget.xpath("value/@currency"))
                     currency = replace_default_if_none(currency,defaults["default-currency"])
                     
-                    donor_code = default_donor_code
-                    donor_text = default_donor_text
+                    provider_org_ref = funding_org_ref
+                    provider_org_text = funding_org_text
+                    provider_org_type = funding_org_type
                     
                     sector_code = defaults["sector"]
-                    # sector = recode_if_not_none(sector_code,vcd["Sector"])
+                    sector = recode_if_not_none(sector_code,vcd["Sector"])
                     
                     recipient_country_code = defaults["recipient-country"]
-                    # recipient_country = recode_if_not_none(recipient_country_code,vcd["Country"])
+                    recipient_country = recode_if_not_none(recipient_country_code,vcd["Country"])
                 
                     recipient_region_code = defaults["recipient-region"]
-                    # recipient_region = recode_if_not_none(recipient_region_code,vcd["Region"])
-                    
-                    flow_type_code = defaults["default-flow-type"]
-                    
-                    finance_type_code = defaults["default-finance-type"]
-                    
-                    aid_type_code = defaults["default-aid-type"]
-                    
-                    category = None
-                    disbursement_channel_code = None
-                    b_or_t = "Budget"
+                    recipient_region = recode_if_not_none(recipient_region_code,vcd["Region"])
             
-                    row = [version,secondary_reporter,year,transaction_date,donor_code,donor_text,recipient_country_code,recipient_region_code,flow_type_code,category,finance_type_code,currency,value,aid_type_code,sector_code,disbursement_channel_code,b_or_t,budget_type]
+                    row = [version,None,None,currency,value,value_date,provider_org_ref,provider_org_text,provider_org_type,None,sector,recipient_country,recipient_region,None,None,None,None,"Budget",budget_type]
                     output.append(row)
     return output
     
 if __name__ == '__main__':
-    rootdir = 'C:/Users/Alex/Documents/Data/IATI-Registry-Refresher/data'
-    for subdir, dirs, files in os.walk(rootdir):
-        for filename in files:
-            filepath = os.path.join(subdir,filename)
-            publisher = os.path.basename(subdir)
-            print filename
-            try:
-                root = etree.parse(filepath).getroot()
-            except etree.XMLSyntaxError:
-                continue
-            output = flatten_activities(root)
-            if len(output)>0:
-                data = pd.DataFrame(output)
-                data.columns = ["version","secondary_reporter","year","transaction_date","donor_code","donor_text","recipient_country_code","recipient_region_code","flow_type_code","category","finance_type_code","currency","value","aid_type_code","sector_code","disbursement_channel_code","b_or_t","budget_type"]
-                data['publisher'] = publisher
-                data.to_csv("C:/Users/Alex/Documents/Data/IATI/sep/{}.csv".format(filename),index=False,header=False,encoding="utf-8")
+    output = []
+    i = 92000
+    while output != 500:
+        output = flatten_datastore_url("http://datastore.iatistandard.org/api/1/access/activity.xml?limit=1000&offset={}".format(i))
+        if output != 500:
+            data = pd.DataFrame(output)
+            # data.columns = ["iati_version","transaction_type","transaction_date","currency","value","value_date","provider_org_ref","provider_org_text","provider_org_type","provider_activity_id","sector","recipient_country","recipient_region","flow_type_code","disbursement_channel","finance_type","aid_type_code","budget_or_transaction","budget_type"]
+            data.to_csv("C:/Users/Alex/Documents/Data/IATI/sep/flat{}.csv".format(i),index=False,header=False,encoding="utf-8")
+            i += 1000
+            print(i)
+    #Reduce limit until we get a valid response
+    lastPos = 1000
+    curPos = lastPos/2
+    lastOutput = 500
+            
+    derivedMax = recursiveSearch(curPos,lastPos,lastOutput)
+    if derivedMax>0:
+        output = flatten_datastore_url("http://datastore.iatistandard.org/api/1/access/activity.xml?limit={}&offset={}".format(derivedMax,i))
+        data = pd.DataFrame(output)
+        # data.columns = ["iati_version","transaction_type","transaction_date","currency","value","value_date","provider_org_ref","provider_org_text","provider_org_type","provider_activity_id","sector","recipient_country","recipient_region","flow_type_code","disbursement_channel","finance_type","aid_type_code","budget_or_transaction","budget_type"]
+        data.to_csv("C:/Users/Alex/Documents/Data/IATI/sep/flat{}.csv".format(i),index=False,header=False,encoding="utf-8")
